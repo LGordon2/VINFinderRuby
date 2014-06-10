e38450884039a8380c1ec02971d09743845dd14d
lew <LGordon2@gmail.com>
2014-06-10 10:33:48 -0400

    Added threading and progress bars

--- grab_vins.rb
+++ grab_vins.rb
@@ -1,5 +1,12 @@
+#Author: Lewis Gordon
+
 require 'open-uri'
 require 'nokogiri'
+require 'ruby-progressbar'
+require 'csv'
+require 'thread/pool'
+
+require File.expand_path('../lib/thread_wait_finished', __FILE__)
 
 def grab_vin(link)
   doc = Nokogiri::HTML(open(link))
@@ -7,19 +14,59 @@ def grab_vin(link)
   nil
 end
 
-threads = []
+# Initialize some variables
 vins = []
+links = []
+pool = Thread.pool(30)
 semaphore = Mutex.new
-doc = Nokogiri::HTML(open('http://www.ebay.com/sch/Cars-Trucks-/6001/i.html?&_trksid=p2050890.m1603'))
-doc.css('table.rsittlref').each do |listing_table|
-  link = listing_table.css('div.ittl a').first[:href]
-  threads << Thread.new do 
+csv_filename = "vins/VINs_#{Time.now.to_i}.csv"
+
+# Grab the vehicle count from the first page.
+doc = Nokogiri::HTML(open(URI::HTTP.build([nil,'www.ebay.com',nil,'/sch/Cars-Trucks-/6001/i.html',nil,nil])))
+vehicle_count = doc.css('div#cbrt div.clt h1.rsHdr span.rcnt').first.content.gsub(',','').to_i
+puts "Searching the first 10000 vehicles of #{vehicle_count}."
+vehicle_count = 10000 if vehicle_count > 10000
+page_count = vehicle_count/50-1
+page_count = 2
+puts "Searching on #{page_count} pages."
+puts 'Finding all vehicle links...'
+
+# Grabbing the individual vehicle links.
+p = ProgressBar.create(total: page_count, :format => '%a %B %p%% %t')
+page_count.times do |num|
+  pool.process do
+    doc = Nokogiri::HTML(open(URI::HTTP.build([nil,'www.ebay.com',nil,'/sch/Cars-Trucks-/6001/i.html',"_pgn=#{num+1}",nil])))
+    listings = doc.css('table.rsittlref')
+    listings.each do |listing_table|
+      found_link = listing_table.css('div.ittl a')
+      links << found_link.first[:href]
+    end
+    p.increment
+  end
+end
+
+pool.wait_until_finished
+
+# Finding the VIN in each link.
+puts 'Finding all VINs...'
+p = ProgressBar.create(total: links.length, :format => '%a %B %p%% %t')
+links.each do |link|
+  pool.process do 
     retrieved_vin = grab_vin(link) 
     semaphore.synchronize do 
       vins << retrieved_vin
+      p.increment
     end
   end
 end
 
-threads.each {|t| t.join}
-puts vins.compact.inspect
+pool.wait_until_finished
+
+# Outputting final results and writing to csv.
+vins = vins.compact
+puts "#{vins.length} VINs retrieved. Writing to [#{csv_filename}]"
+CSV.open(csv_filename,'wb') do |csv|
+  vins.each do |vin|
+    csv << [vin]
+  end
+end
