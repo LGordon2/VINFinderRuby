ed8d1c8533d642fe97f0d57cea0ec8494903d2be
Lew Gordon <LGordon2@gmail.com>
2014-08-28 09:40:31 -0400

    eBay changed and so did the script.

--- grab_vins.rb
+++ grab_vins.rb
@@ -6,13 +6,21 @@ require 'nokogiri'
 require 'ruby-progressbar'
 require 'csv'
 require 'thread/pool'
+require 'logger'
 
 require File.expand_path('../lib/thread_wait_finished', __FILE__)
 require File.expand_path('../lib/params',__FILE__)
 
+logger = Logger.new(STDOUT)
+logger.level = Logger::INFO
+
 def grab_vin(link)
   doc = Nokogiri::HTML(open(link))
-  doc.css("div.itemAttr table:last-child tr").each {|row| row.css("td").each {|node| return node.next_element.content.strip if node.content.include? "VIN" or node.content.include? "Hull ID" } }
+  doc.css("div.itemAttr table:last-child tr").each do |row|
+    row.css("td").each do |node|
+      return node.next_element.content.strip if node.content.include?("VIN") or node.content.include?("Hull ID") 
+    end
+  end
   nil
 end
 
@@ -37,13 +45,15 @@ puts "Searching on #{page_count} pages."
 puts 'Finding all vehicle links...'
 
 # Grabbing the individual vehicle links.
-p = ProgressBar.create(total: page_count, :format => '%a %B %p%% %t')
+p = ProgressBar.create(total: page_count, format: '%a %B %p%% %t')
 page_count.times do |num|
   pool.process do
-    doc = Nokogiri::HTML(open(uri.set_params("_pgn" => num+1)))
-    listings = doc.css('table.rsittlref')
+    doc = Nokogiri::HTML(open(uri.set_params("_pgn" => num)))
+    listings = doc.css('#ResultSetItems #ListViewInner li .lvtitle')
+    logger.debug listings.size
     listings.each do |listing_table|
-      found_link = listing_table.css('div.ittl a')
+      found_link = listing_table.css('a')
+      logger.debug found_link.first[:href]
       links << found_link.first[:href]
     end
     p.increment
@@ -54,7 +64,7 @@ pool.wait_until_finished
 
 # Finding the VIN in each link.
 puts 'Finding all VINs...'
-p = ProgressBar.create(total: links.length, :format => '%a %B %p%% %t')
+p = ProgressBar.create(total: links.length, format: '%a %B %p%% %t')
 links.each do |link|
   pool.process do 
     retrieved_vin = grab_vin(link) 
@@ -70,7 +80,7 @@ pool.wait_until_finished
 # Outputting final results and writing to csv.
 vins = vins.compact.uniq
 puts "#{vins.length} VINs retrieved. Writing to [#{csv_filename}]"
-puts "Retrieval success: #{(((vins.length.to_f) / (vehicle_count.to_f)) * 100.0).round(2)}%"
+puts "Retrieval success: #{(((vins.length.to_f) / (links.size.to_f)) * 100.0).round(2)}%"
 CSV.open(csv_filename,'wb') do |csv|
   vins.each do |vin|
     csv << [vin]
